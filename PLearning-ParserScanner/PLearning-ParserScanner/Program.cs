d8ca4c2a2093204561ab1fe1737b32ab272bc045
MarceloCQ <macq_94@hotmail.com>
2016-03-02 11:11:56 -0600

    Primer Commit

---
+++ PLearning-ParserScanner/PLearning-ParserScanner/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PLearning_ParserScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
